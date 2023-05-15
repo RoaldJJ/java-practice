a4d4044555abf9d78056a5f3ddd7dfb243daa53c
RoaldJJ <133646332+RoaldJJ@users.noreply.github.com>
2023-05-15 12:11:55 +0200

    ????

---
+++ new2/src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf(
+                "Willem, die vele bouke maecte,\n" +
+                "Daer hi dicken omme waecte,\n" +
+                "Hem vernoyde so haerde\n" +
+                "Dat die avonture van Reynaerde\n" +
+                "In Dietsche onghemaket bleven\n"
+        );
+
+    }
+}
