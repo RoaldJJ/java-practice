dd4c1574d168bcc3b2ae2a806bda10ec7a67c317
RoaldJJ <133646332+RoaldJJ@users.noreply.github.com>
2023-05-15 14:45:23 +0200

    1

--- new2/src/Main.java
+++ new2/src/Main.java
@@ -2,14 +2,6 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-        System.out.printf(
-                "Willem, die vele bouke maecte,\n" +
-                "Daer hi dicken omme waecte,\n" +
-                "Hem vernoyde so haerde\n" +
-                "Dat die avonture van Reynaerde\n" +
-                "In Dietsche onghemaket bleven\n"
-        );
-
+        Reynarde reynard = new Reynarde();
     }
 }
---
+++ new2/src/Reynarde.java
@@ -0,0 +1,13 @@
+public class Reynarde {
+    public Reynarde() {
+        System.out.print(
+                """
+                Willem, die vele bouke maecte,
+                Daer hi dicken omme waecte,
+                Hem vernoyde so haerde
+                Dat die avonture van Reynaerde
+                In Dietsche onghemaket bleven
+                """
+        );
+    }
+}
